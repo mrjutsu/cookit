7e9e3fe85e4384c26305d4d581437621ee7b2a10
David Rosillo <drr538@gmail.com>
2014-10-04 12:29:38 -0500

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config/environments/production.rb
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => 'cookits3bucket',
-    :access_key_id => 'AKIAJ7NIVK3DYX4IHP5A',
-    :secret_access_key => '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'
-  }
-}
 
 end
